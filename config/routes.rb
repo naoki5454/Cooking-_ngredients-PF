b85d7451381422ba3a6a13d936d9401c537dccd1
naoki5454 <naoking250912@icloud.com>
2021-04-05 13:46:14 +0000

    ルーティンング完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
+
+  root to:  'homes#top'
+  get '/about' => 'homes#about'
+
   devise_for :admins
+
+  namespace :admins do
+    resources :customers, only: [:show, :index, :edit, :update, :destroy]
+    delete 'cuisines/:id' => 'cuisines#destroy', as: 'cuisine'
+    resources :genres, only: [:show, :index, :edit, :update, :destroy]
+  end
+
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    resources :customers, only: [:show, :edit, :update]
+    get 'confirm/:id' => 'customers#confirm', as: 'destroy_confirm'
+    patch 'withdraw/:id' => 'customers#withdraw', as: 'withdraw_customer'
+    resources :cuisines, only: [:new, :show, :create, :index, :edit, :update, :destroy] do
+      resource :cuisine_favorites, only: [:create, :destroy]
+      resources :cuisine_comments, only: [:create, :destroy] #もしかしたらできないかも
+    end
+  end
+
+
 end
