1827620626281f868365dc9a7a417a65684c3127
naoki5454 <naoking250912@icloud.com>
2021-04-06 12:36:11 +0000

    ジャンルページ作成中

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,21 @@
 class Admins::GenresController < ApplicationController
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admins_genres_path
+    else
+      render admins_genres_path
+    end
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
