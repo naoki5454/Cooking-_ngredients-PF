ac8dfad780c898e29562955ff0b15a35d97e6a09
naoki5454 <naoking250912@icloud.com>
2021-04-07 07:48:16 +0000

    料理の投稿時にロールバックのエラー

--- app/controllers/public/cuisines_controller.rb
+++ app/controllers/public/cuisines_controller.rb
@@ -1,2 +1,22 @@
 class Public::CuisinesController < ApplicationController
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(cuisine_params)
+    #if
+    @cuisine.save
+      redirect_to cuisines_path
+    #else
+      #render "new"
+    #end
+  end
+
+  private
+  def cuisine_params
+    params.require(:cuisine).permit(:cuisine_image_id, :genre_id, :cuisine_name, :introduction, :time)
+  end
+
 end
