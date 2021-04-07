590f0c1b990f933090ba14d47997bea767bb2405
naoki5454 <naoking250912@icloud.com>
2021-04-07 05:46:14 +0000

    モデルに記述の追加

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -5,6 +5,7 @@ class Cuisine < ApplicationRecord
   validates :introduction, presence: true
   validates :time, presence: true
 
+  belongs_to :customer
   has_many :cuisine_comments, dependent: :destroy
   has_many :cuisine_favorites, dependent: :destroy
 end
