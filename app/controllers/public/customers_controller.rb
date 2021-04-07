fa0248257fc5d3be69a3a6266fbff5db61ad1889
naoki5454 <naoking250912@icloud.com>
2021-04-07 05:46:14 +0000

    モデルに記述の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,10 @@
 class Public::CustomersController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+    @cuisines = @customer.cuisines
+  end
 end
