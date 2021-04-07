300156269fcc7f263ca2b87969689a533e4b765c
naoki5454 <naoking250912@icloud.com>
2021-04-07 04:16:32 +0000

    admin

--- app/controllers/admins/cuisines_controller.rb
+++ app/controllers/admins/cuisines_controller.rb
@@ -1,2 +1,14 @@
 class Admins::CuisinesController < ApplicationController
+
+  def index
+    @cuisines = Cuisine.page(params[:page]).per(8)
+  end
+
+  def show
+    @cuisine = Cuisine.find(params[:id])
+  end
+
+  def destroy
+    @cuisine = Cuisine.find(params[:id])
+  end
 end
