d7013cb1c54659ea77823a88a134dfecb3cebbd1
naoki5454 <naoking250912@icloud.com>
2021-04-10 01:42:31 +0000

    コントローラー

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,4 +5,17 @@ class Public::CustomersController < ApplicationController
     @cuisines = @customer.cuisines
   end
 
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
 end
