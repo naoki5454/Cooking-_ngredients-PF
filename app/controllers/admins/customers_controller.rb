0ba63d0394e1786e43026a03a1e02f2c908d33da
naoki5454 <naoking250912@icloud.com>
2021-04-12 04:02:16 +0000

    admin会員情報編集ページ完成

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -9,10 +9,18 @@ class Admins::CustomersController < ApplicationController
   end
 
   def edit
-
+    @customer = Customer.find(params[:id])
   end
 
   def update
-
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admins_customer_path(@customer), notice: "更新しました"
   end
+
+   private
+  def customer_params
+    params.require(:customer).permit(:name, :k_name, :k_las, :email, :is_valid)
+  end
+
 end
