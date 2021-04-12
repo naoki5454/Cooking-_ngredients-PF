b8717e8969c6b3a5b5582f50c69d8fcd783f9a70
naoki5454 <naoking250912@icloud.com>
2021-04-12 01:18:56 +0000

    会員一覧作成中

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,18 @@
 class Admins::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
