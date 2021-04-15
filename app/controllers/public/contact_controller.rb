cc723ef666fffc82a95416e638ca3f5a6076a1ac
naoki5454 <naoking250912@icloud.com>
2021-04-15 04:38:32 +0000

    やり方変更

--- app/controllers/public/contact_controller.rb
+++ app/controllers/public/contact_controller.rb
@@ -7,19 +7,16 @@ class Public::ContactController < ApplicationController
 
   def confirm
     # 入力値のチェック
-    @contact = Contact.new(params[:inquiry])
-    if @contact.valid?
-      # OK。確認画面を表示
+    contact = params.require(:contact).permit(:name, :email, :message)
+    @contact = Contact.new(contact)
+      #確認画面を表示
       render 'confirm'
-    else
-      # NG。入力画面を再表示
-      render 'new'
-    end
   end
 
   def thanks
     # メール送信
-    @contact = Contact.new(params[:inquiry])
+    contact = params.require(:contact).permit(:name, :email, :message)
+    @contact = Contact.new(contact)
     InquiryMailer.received_email(@contact).deliver
 
     # 完了画面を表示
