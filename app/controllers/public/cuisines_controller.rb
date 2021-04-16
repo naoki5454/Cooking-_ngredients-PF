932672e26e39f68404d82fc3c70e0549311768e5
naoki5454 <naoking250912@icloud.com>
2021-04-16 12:13:30 +0000

    placeholderの変更、レイアウト整え

--- app/controllers/public/cuisines_controller.rb
+++ app/controllers/public/cuisines_controller.rb
@@ -20,7 +20,7 @@ class Public::CuisinesController < ApplicationController
   end
 
   def index
-    @cuisines = Cuisine.page(params[:page]).per(8)
+    @cuisines = Cuisine.page(params[:page]).per(4)
     @genres = Genre.page(params[:page]).per(8)
   end
 
