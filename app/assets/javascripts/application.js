c69584db9bb92e3819c7dda640871baef1945a00
naoki5454 <naoking250912@icloud.com>
2021-04-19 17:00:06 +0000

    ハンバーガー完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,3 +51,15 @@ $("#theTarget").skippr({
 });
 });
 });
+
+ $(function () {
+    $("#nav-open").on("click", function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $("#nav-content").removeClass("open").fadeOut(100);
+      } else {
+        $(this).addClass("active");
+        $("#nav-content").fadeIn(100).addClass("open");
+      }
+    });
+  });
