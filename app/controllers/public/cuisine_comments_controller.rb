896a7f4651ef9cf90e186a9cfe837b31db902d53
naoki5454 <naoking250912@icloud.com>
2021-04-22 06:04:14 +0000

    非同期のエラーメッセージ完了

--- app/controllers/public/cuisine_comments_controller.rb
+++ app/controllers/public/cuisine_comments_controller.rb
@@ -5,10 +5,10 @@ class Public::CuisineCommentsController < ApplicationController
     @cuisine = Cuisine.find(params[:cuisine_id])
     @comment = current_customer.cuisine_comments.new(cuisine_comments_params)
     @comment.cuisine_id = @cuisine.id
-    if @comment.save
+    @comment.customer_id = current_customer.id
+    unless @comment.save
       #redirect_to cuisine_path(cuisine)
-    else
-      render 'show'
+      render 'errors'
     end
   end
 
@@ -22,7 +22,7 @@ class Public::CuisineCommentsController < ApplicationController
   private
 
   def cuisine_comments_params
-    params.require(:cuisine_comment).permit(:comment, :cuisine_id, :customer_id)
+    params.require(:cuisine_comment).permit(:comment)
   end
 
 end
