629b0d5ff2fb67e9b3c183c6c9578c78b5ee29de
naoki5454 <naoking250912@icloud.com>
2021-04-28 03:22:14 +0000

    最終調整完了

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -2,7 +2,7 @@ class Admins::ContactsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @contacts = Contact.all
+    @contacts = Contact.page(params[:page]).per(8)
   end
 
   def show
